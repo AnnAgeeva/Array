9d74f4ff069ecd3eda5fc721a5999cc52d12f7d4
AnnAgeeva <anka-287@yandex.ru>
2024-08-03 22:46:01 +0300

    Добавьте файлы проекта.

---
+++ Array/Program.cs
@@ -0,0 +1,25 @@
+﻿
+Console.WriteLine("Случайная матрица");
+Console.WriteLine();
+Console.WriteLine("Введите количество строк матрицы");
+int i = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов матрицы");
+int j = int.Parse(Console.ReadLine());
+Random r = new Random();
+
+int[,] matrix = new int[i, j];
+int sum = 0;
+for (i = 0; i < matrix.GetLength(0); i++)
+{
+
+    for (j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i, j] = r.Next(10);
+        Console.Write($"{matrix[i, j]}  ");
+        sum = sum + matrix[i, j];
+    }
+    Console.WriteLine();
+
+}
+Console.WriteLine("Сумма элементов матрицы = " + sum);
+Console.ReadKey();
---
+++ summatrix/Program.cs
@@ -0,0 +1,45 @@
+﻿Console.WriteLine("Сложение матриц");
+Console.WriteLine();
+Console.WriteLine("Введите количество строк матрицы");
+int i = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов матрицы");
+int j = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+Random r = new Random();
+
+int[,] a = new int[i, j];
+int[,] b = new int[i, j];
+for (i = 0; i < a.GetLength(0); i++)
+{
+
+    for (j = 0; j < a.GetLength(1); j++)
+    {
+        a[i, j] = r.Next(10);
+        Console.Write($"{a[i, j]}  ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+for (i = 0; i < b.GetLength(0); i++)
+{
+    for (j = 0; j < b.GetLength(1); j++)
+    {
+        b[i, j] = r.Next(10);
+        Console.Write($"{b[i, j]}  ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+Console.WriteLine("Сумма матриц");
+int[,] c = new int[i, j];
+for (i = 0; i < c.GetLength(0); i++)
+{
+    for (j = 0; j < c.GetLength(1); j++)
+    {
+        c[i, j] = a[i, j] + b[i, j];
+        Console.Write($"{c[i, j]}  ");
+    }
+    Console.WriteLine();
+}
